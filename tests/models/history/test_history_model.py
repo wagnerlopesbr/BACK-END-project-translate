60c0babde0dc2e7102acca9e38793f6058dc92ff
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-05-19 12:03:24 -0300

    Req ?/10 v1

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,16 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
-def test_request_history():
-    raise NotImplementedError
+def test_request_history(prepare_base):
+    history_translations_str = HistoryModel.list_as_json()
+    history_translations = json.loads(history_translations_str)
+    assert any(
+        entry["text_to_translate"] == "Hello, I like videogame"
+        for entry in history_translations
+    )
+    assert any(
+        entry["text_to_translate"] == "Do you love music?"
+        for entry in history_translations
+    )
